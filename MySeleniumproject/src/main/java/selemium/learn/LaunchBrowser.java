ad6c79bfee0e15cd0babffa74a6f200d1169e698
Shubh <spp021992@gmail.com>
2024-09-01 14:50:24 -0500

    Added example of explicit wait & created class for readpara to work in future

--- MySeleniumproject/src/main/java/selemium/learn/LaunchBrowser.java
+++ MySeleniumproject/src/main/java/selemium/learn/LaunchBrowser.java
@@ -1,20 +1,44 @@
 package selemium.learn;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LaunchBrowser {
 
+
+	static WebDriver driver;
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/driver/chromedriver.exe");
 		driver.manage().window().maximize();
 		driver.get("https://amazon.in");
 
+		
+		/* **********************************************************************************************
+		 * This is an example of Explicit wait where driver will wait until amazon logo will display. 
+		 * if displayed then it will move to next step or show timeout.
+		 * **********************************************************************************************
+		 */
+		WebElement Amazonlogo= driver.findElement(By.id("nav-logo-sprites"));
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(30));
+		wait.until(ExpectedConditions.visibilityOf(Amazonlogo));
+		
+		WebElement strStartherelink=driver.findElement(By.xpath("//a[@class=\"sign-in-tooltip-link\"]"));
+		wait.until(ExpectedConditions.elementToBeClickable(strStartherelink));
+		
+		/*
+		 ************************************************************************************************** 
+		 */
+		
 		String strSearch="Apple iphone 15";
 		WebElement searchbox=driver.findElement(By.id("twotabsearchtextbox"));
 		searchbox.sendKeys(strSearch);
